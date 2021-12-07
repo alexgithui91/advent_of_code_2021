89c0baf07fcf775aaa87f26912a76d0f2a7bb931
alexgithui91 <alexgithui91@gmail.com>
2021-12-07 15:52:15 +0300

    complete part i

--- day1.py
+++ day1.py
@@ -1,20 +1,25 @@
 import csv
 
-with open("day1_input.csv", newline="") as f:
-    reader = csv.reader(f)
-    measurements_list = list(reader)
 
-positives = 0
+def part_one():
+    with open("day1_input.csv", newline="") as f:
+        reader = csv.reader(f)
+        measurements_list = list(reader)
 
-for itr in range(len(measurements_list)):
-    if itr == 0:
-        # print("N/A - no previous measurement")
-        pass
-    else:
-        val1 = int(measurements_list[itr][0])
-        val2 = int(measurements_list[itr - 1][0])
-        diff = val1 - val2
-        if diff > 0:
-            positives += 1
+    positives = 0
 
-print(positives)
+    for itr in range(len(measurements_list)):
+        if itr == 0:
+            # print("N/A - no previous measurement")
+            pass
+        else:
+            val1 = int(measurements_list[itr][0])
+            val2 = int(measurements_list[itr - 1][0])
+            diff = val1 - val2
+            if diff > 0:
+                positives += 1
+
+    return positives
+
+
+print(part_one())
