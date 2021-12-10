ab944e633cf7dd7e361a3b2bf03f1e34a2222271
alexgithui91 <alexgithui91@gmail.com>
2021-12-10 14:44:54 +0300

    working day2 advent of code

--- day2.py
+++ day2.py
@@ -1,1 +1,58 @@
 import csv
+
+
+def part_one():
+
+    with open("day2_input.csv", newline="") as f:
+        reader = csv.reader(f)
+        navigation_list = list(reader)
+
+    horizontal = 0
+    depth = 0
+
+    for row in navigation_list:
+        direction = row[0].split(" ")[0]
+        distance = int(row[0].split(" ")[1])
+
+        if direction == "forward":
+            horizontal += distance
+        if direction == "down":
+            depth += distance
+        if direction == "up":
+            depth -= distance
+
+    print(horizontal * depth)
+
+
+def part_two():
+
+    with open("day2_input.csv", newline="") as f:
+        reader = csv.reader(f)
+        navigation_list = list(reader)
+
+    horizontal = 0
+    depth = 0
+    aim = 0
+
+    for row in navigation_list:
+        direction = row[0].split(" ")[0]
+        distance = int(row[0].split(" ")[1])
+
+        if direction == "forward":
+            horizontal += distance
+            depth += aim * distance
+        if direction == "down":
+            aim += distance
+        if direction == "up":
+            aim -= distance
+
+    print(horizontal * depth)
+
+
+def main():
+    # part_one()
+    part_two()
+
+
+if __name__ == "__main__":
+    main()
