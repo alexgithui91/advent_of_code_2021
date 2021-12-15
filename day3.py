09d5ae68b40f2cdc22a9f5b71f6171ccf2203cbb
alexgithui91 <alexgithui91@gmail.com>
2021-12-15 15:19:40 +0300

    successfully pulling most common

--- day3.py
+++ day3.py
@@ -1,18 +1,35 @@
 import csv
 
-# def most_frequent(List):
-#     return max(set(List), key=List.count)
-
-
-# List = [2, 1, 2, 2, 1, 3]
-# print(most_frequent(List))
-
 
 def part_one():
+    col1, col2, col3, col4, col5 = [], [], [], [], []
+
     with open("day3_input.csv", newline="") as f:
         reader = csv.reader(f)
         binary_list = list(reader)
-    print(bi)
+
+    for itm in binary_list:
+        col1.append(int(itm[0][0]))
+        col2.append(int(itm[0][1]))
+        col3.append(int(itm[0][2]))
+        col4.append(int(itm[0][3]))
+        col5.append(int(itm[0][4]))
+
+    most_comm_1 = max(set(col1), key=col1.count)
+    most_comm_2 = max(set(col2), key=col2.count)
+    most_comm_3 = max(set(col3), key=col3.count)
+    most_comm_4 = max(set(col4), key=col4.count)
+    most_comm_5 = max(set(col5), key=col5.count)
+
+    most_comm_binary = (
+        str(most_comm_1)
+        + str(most_comm_2)
+        + str(most_comm_3)
+        + str(most_comm_4)
+        + str(most_comm_5)
+    )
+
+    most_comm_decimal = int(most_comm_binary, 2)
 
 
 def part_two():
