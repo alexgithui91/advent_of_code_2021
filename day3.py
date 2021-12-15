b05a505489a0ff03e17ed08b3e42749b07b33e73
alexgithui91 <alexgithui91@gmail.com>
2021-12-15 09:11:57 +0300

    started day3

--- day3.py
+++ day3.py
@@ -1,1 +1,28 @@
-print("This is a test")
+import csv
+
+# def most_frequent(List):
+#     return max(set(List), key=List.count)
+
+
+# List = [2, 1, 2, 2, 1, 3]
+# print(most_frequent(List))
+
+
+def part_one():
+    with open("day3_input.csv", newline="") as f:
+        reader = csv.reader(f)
+        binary_list = list(reader)
+    print(bi)
+
+
+def part_two():
+    pass
+
+
+def main():
+    part_one()
+    # part_two()
+
+
+if __name__ == "__main__":
+    main()
