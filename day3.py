e109bb5597472dc05e29cfdd10562d07b8ed3cd0
alexgithui91 <alexgithui91@gmail.com>
2021-12-15 16:23:23 +0300

    working day3.py dynamic

--- day3.py
+++ day3.py
@@ -2,50 +2,24 @@ import csv
 
 
 def part_one():
-    col1, col2, col3, col4, col5 = [], [], [], [], []
+    most_comm_binary = ""
+    least_comm_binary = ""
 
     with open("day3_input.csv", newline="") as f:
         reader = csv.reader(f)
-        binary_list = list(reader)
+        tmp_list = list(reader)
 
-    for itm in binary_list:
-        col1.append(int(itm[0][0]))
-        col2.append(int(itm[0][1]))
-        col3.append(int(itm[0][2]))
-        col4.append(int(itm[0][3]))
-        col5.append(int(itm[0][4]))
+    binary_list = [[] for i in range(len(tmp_list[0][0]))]
 
-    most_comm_1 = max(set(col1), key=col1.count)
-    most_comm_2 = max(set(col2), key=col2.count)
-    most_comm_3 = max(set(col3), key=col3.count)
-    most_comm_4 = max(set(col4), key=col4.count)
-    most_comm_5 = max(set(col5), key=col5.count)
+    for lst_cnt in range(len(binary_list)):
+        for itm in tmp_list:
+            binary_list[lst_cnt].append(int(itm[0][lst_cnt]))
 
-    most_comm_binary = (
-        str(most_comm_1)
-        + str(most_comm_2)
-        + str(most_comm_3)
-        + str(most_comm_4)
-        + str(most_comm_5)
-    )
-    most_comm_decimal = int(most_comm_binary, 2)
+    for lst_itm in binary_list:
+        most_comm_binary += str(max(set(lst_itm), key=lst_itm.count))
+        least_comm_binary += str(min(set(lst_itm), key=lst_itm.count))
 
-    least_comm_1 = min(set(col1), key=col1.count)
-    least_comm_2 = min(set(col2), key=col2.count)
-    least_comm_3 = min(set(col3), key=col3.count)
-    least_comm_4 = min(set(col4), key=col4.count)
-    least_comm_5 = min(set(col5), key=col5.count)
-
-    least_comm_binary = (
-        str(least_comm_1)
-        + str(least_comm_2)
-        + str(least_comm_3)
-        + str(least_comm_4)
-        + str(least_comm_5)
-    )
-    least_comm_decimal = int(least_comm_binary, 2)
-
-    print(most_comm_decimal * least_comm_decimal)
+    print(int(most_comm_binary, 2) * int(least_comm_binary, 2))
 
 
 def part_two():
